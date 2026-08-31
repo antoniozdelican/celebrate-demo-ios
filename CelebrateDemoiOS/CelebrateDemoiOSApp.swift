d5164fd5a8c7f0c51935156cb3f918e6db516300
Antonio Zdelican <antonio.zdelican@gmail.com>
2026-08-31 11:49:25 +0200

    Initial Commit

---
+++ CelebrateDemoiOS/CelebrateDemoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelebrateDemoiOSApp.swift
+//  CelebrateDemoiOS
+//
+//  Created by Antonio Zdelican on 31.08.2026..
+//
+
+import SwiftUI
+
+@main
+struct CelebrateDemoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CelebrateDemoiOSTests/CelebrateDemoiOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  CelebrateDemoiOSTests.swift
+//  CelebrateDemoiOSTests
+//
+//  Created by Antonio Zdelican on 31.08.2026..
+//
+
+import Testing
+
+struct CelebrateDemoiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
